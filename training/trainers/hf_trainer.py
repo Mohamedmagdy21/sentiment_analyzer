d3a013b4efdc1da5d00524e3a364437521aacddd
Mohamedmagdy21 <mohamedmagdy10011@gmail.com>
2026-06-11 16:46:59 +0300

    fix: reduce epochs 3->1 to fit Kaggle 12h session limit

--- training/trainers/hf_trainer.py
+++ training/trainers/hf_trainer.py
@@ -8,16 +8,6 @@ from transformers import (
     TrainingArguments
 )
 
-# Patch transformers 5.x _get_num_items_in_batch for T4 GPU compat
-from transformers import trainer as trainer_module
-orig_get_num_items = trainer_module.Trainer._get_num_items_in_batch
-def patched_get_num_items(self, batch_samples, device):
-    try:
-        return orig_get_num_items(self, batch_samples, device)
-    except Exception:
-        return sum(batch["labels"].numel() for batch in batch_samples)
-trainer_module.Trainer._get_num_items_in_batch = patched_get_num_items
-
 from training.trainers.base_trainer import BaseTrainer
 import mlflow
 import mlflow.transformers
@@ -88,7 +78,7 @@ class HuggingFaceTrainer(BaseTrainer):
 
             output_dir="artifacts/checkpoints",
 
-            num_train_epochs=3,
+            num_train_epochs=1,
 
             per_device_train_batch_size=16,
 
